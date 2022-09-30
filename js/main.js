033b9569c1921e597b54e8ab17b8f02a0aed58a7
Олег <matrix425@yandex.ru>
2022-09-30 11:33:37 +0300

    Вносит правки в функции

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-//Должна ли функция getRandomInteger принимать не целые числа?
-//На сколкьо такой вариант написания приемлем: const getRandomInteger = (fromNumber, beforeNumber) => !(fromNumber <= 0 || beforeNumber <= 0 || beforeNumber < fromNumber) ? NaN : Math.floor(Math.random() * (fromNumber - beforeNumber + 1)) + beforeNumber;
-//Функцияю взял с ресурса https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random - у меня достаточно глупый вопрос, аж стдно: зачем +1 в расчетах???
+//Функцияю взял с ресурса https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+/*Из ТЗ :Придумайте, как функция будет вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.
+Например, в этом случае функция также может возвращать NaN Или же вы можете проверить, какой из аргументов больше, а какой меньше, и при необходимости поменять их местами.
+- в принципе ничего сложного как я понима делаем следующее :
+(beforeNumber < fromNumber)
+? let swap = beforeNumber
+beforeNumber = fromNumber
+fromNumber = swap
+но я не понимаю как это все правильно записать тернарными операторами, крутил по всякому*/
 
-const getRandomInteger = (fromNumber, beforeNumber) => {
-  if(fromNumber <= 0 || beforeNumber <= 0 || beforeNumber < fromNumber) {
-    return NaN;
-  }
-  return Math.floor(Math.random() * (fromNumber - beforeNumber + 1)) + beforeNumber;
-};
+const getRandomInteger = (fromNumber, beforeNumber) => (
+  fromNumber <= 0 || beforeNumber <= 0 || beforeNumber <= fromNumber || !Number.isInteger(fromNumber) || !Number.isInteger(beforeNumber)
+    ? NaN
+    : Math.floor(Math.random() * (fromNumber - beforeNumber + 1)) + beforeNumber
+);
 
 getRandomInteger();
 
-const stringLength = (string, maxLength) => !((string > maxLength));
+const isMaxLength = (string, maxLength) => (maxLength >= string);
 
-stringLength();
+isMaxLength();
