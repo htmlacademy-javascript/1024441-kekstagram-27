e0a3d24ef00d00209940d602d648b3154de8fb26
Олег <matrix425@yandex.ru>
2022-09-30 13:39:22 +0300

    Вносит правки в функции, подключает файл js на главную страницу

--- js/main.js
+++ js/main.js
@@ -16,6 +16,6 @@ const getRandomInteger = (fromNumber, beforeNumber) => (
 
 getRandomInteger();
 
-const isMaxLength = (string, maxLength) => (maxLength >= string);
+const isMaxLength = (string, maxLength) => (maxLength >= string.length);
 
 isMaxLength();
