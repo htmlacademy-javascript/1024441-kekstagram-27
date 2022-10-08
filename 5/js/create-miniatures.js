b4be876c61661248f3bd40b7b2229529ebe8191f
Keks <keks@htmlacademy.ru>
2022-10-08 12:09:48 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/create-miniatures.js
@@ -0,0 +1,19 @@
+import {createObjects} from './data.js';
+
+const pictures = document.querySelector('.pictures');
+const templateMiniatures = document.querySelector('#picture').content.querySelector('.picture');
+const similarMiniatures = createObjects();
+const similarListMiniature = document.createDocumentFragment();
+
+const createMiniatures = () => {
+  similarMiniatures.forEach((({url, likes, comments}) => {
+    const miniature = templateMiniatures.cloneNode(true);
+    miniature.querySelector('.picture__img').src = url;
+    miniature.querySelector('.picture__comments').textContent = comments.length;
+    miniature.querySelector('.picture__likes').textContent = likes;
+    similarListMiniature.appendChild(miniature);
+  }));
+  pictures.appendChild(similarListMiniature);
+};
+
+export {createMiniatures};
