622ab3c52e49bf36aa775c3eb8f5cad8cae57551
Олег <matrix425@yandex.ru>
2022-10-25 16:05:49 +0300

    Реализует функционал и логику фильтров

--- js/image-editing.js
+++ js/image-editing.js
@@ -2,6 +2,11 @@ const SCALE_STEP = 25;
 const MIN_VALUE_SCALE_CONTROL = 25;
 const MAX_VALUE_SCALE_CONTROL = 100;
 const GET_PERCENT = 100;
+const EFFECT_CHROME_STEP = 0.1;
+const EFFECT_SEPIA_STEP = 0.1;
+const EFFECT_MARVIN_STEP = 1;
+const EFFECT_PHOBOS_STEP = 0.1;
+const EFFECT_HEAT_STEP = 0.1;
 
 const photoScale = (modal) => {
   const scaleForm = modal.querySelector('.scale');
@@ -32,45 +37,134 @@ const photoScale = (modal) => {
   });
 
   const sliderElement = document.querySelector('.effect-level__slider');
+  const effectsList = modal.querySelector('.effects__list');
+  const valueElement = modal.querySelector('.effect-level__value');
+  const originalEffects = modal.querySelector('#effect-none');
+
+  // стоит ли завести новый модуль в котором будет храниться библиотека эффектов ?
+
+  const effects = {
+    none: {
+      range: {
+        min: 0,
+        max: 100,
+      },
+      start: 100,
+      step: '',
+      class: 'effects__preview--none',
+      filter: ' ',
+      simbol: ''
+    },
+    chrome: {
+      range: {
+        min: 0,
+        max: 1
+      },
+      start: 1,
+      step: EFFECT_CHROME_STEP,
+      class: 'effects__preview--chrome',
+      filter: 'grayscale',
+      simbol: ''
+    },
+    sepia: {
+      range: {
+        min: 0,
+        max: 1
+      },
+      start: 1,
+      step: EFFECT_SEPIA_STEP,
+      class: 'effects__preview--sepia',
+      filter: 'sepia',
+      simbol: ''
+    },
+    marvin: {
+      range: {
+        min: 0,
+        max: 100
+      },
+      start: 100,
+      step: EFFECT_MARVIN_STEP,
+      class: 'effects__preview--marvin',
+      filter: 'invert',
+      simbol: '%'
+    },
+    phobos: {
+      range: {
+        min: 0,
+        max: 3
+      },
+      start: 100,
+      step: EFFECT_PHOBOS_STEP,
+      class: 'effects__preview--phobos',
+      filter: 'blur',
+      simbol: 'px'
+    },
+    heat: {
+      range: {
+        min: 1,
+        max: 3
+      },
+      start: 3,
+      step: EFFECT_HEAT_STEP,
+      class: 'effects__preview--heat',
+      filter: 'brightness',
+      simbol: ''
+    }
+  };
+
 
   noUiSlider.create(sliderElement, {
     range: {
       min: 0,
       max: 100,
     },
-    start: 80,
+    start: 100,
     step: 1,
     connect: 'lower',
+  });
+
+  const isOriginalEffect = () => {
+    if(originalEffects.checked) {
+      return sliderElement.classList.add('hidden');
+    }return sliderElement.classList.remove('hidden');
+  };
+
+  // по хорошему слайдер должен вообще удаляться, но у меня так ине получилось реализовать это
+  // что я пробовал :
+  // const isOriginalEffects = () => {
+  //   if(originalEffects.checked) {
+  //    return sliderElement.noUiSlider.destroy();
+  //   }return noUiSlider.create(sliderElement, {
+  //     range: {
+  //       min: 0,
+  //       max: 100,
+  //     },
+  //     start: 100,
+  //     step: 1,
+  //     connect: 'lower',
+  //   });;
+  // };
+  // и разные вариации, думал через ключ - у которого значение будет булевым - его передавать через .setAttribute()
+
+
+  isOriginalEffect();
+  effectsList.addEventListener('change', (evt) => {
+    isOriginalEffect();
+    photo.className = '';
+    photo.style.filter = 'none';
+    sliderElement.noUiSlider.updateOptions({
+      range: effects[evt.target.value].range,
+      start: effects[evt.target.value].start,
+      step: effects[evt.target.value].step,
+    });
+    photo.classList.add(effects[evt.target.value].class);
+    sliderElement.noUiSlider.on('update', () => {
+      valueElement.value = sliderElement.noUiSlider.get();
+      photo.style.filter = `${effects[evt.target.value].filter}(${valueElement.value}${effects[evt.target.value].simbol})`;
+    });
   });
 };
 
 export {
   photoScale
 };
-// // С помощью библиотеки noUiSlider (/vendor/nouislider) реализуйте применение
-// эффекта для изображения. Кроме визуального применения эффекта необходимо записывать
-// значение в скрытое поле для дальнейшей отправки на сервер.
-
-//Наложение эффекта на изображение:
-
-// По умолчанию должен быть выбран эффект «Оригинал».
-// На изображение может накладываться только один эффект.
-// При смене эффекта, выбором одного из значений среди радиокнопок .effects__radio,
-// добавить картинке внутри .img-upload__preview CSS-класс, соответствующий эффекту.
-// Например, если выбран эффект .effect-chrome, изображению нужно добавить класс effects__preview--chrome.
-// Интенсивность эффекта регулируется перемещением ползунка в слайдере. Слайдер реализуется`
-// сторонней библиотекой для реализации слайдеров noUiSlider. Уровень эффекта записывается в поле .
-// effect-level__value. При изменении уровня интенсивности эффекта (предоставляется API слайдера),
-// CSS-стили картинки внутри .img-upload__preview обновляются следующим образом:
-// Для эффекта «Хром» — filter: grayscale(0..1) с шагом 0.1;
-// Для эффекта «Сепия» — filter: sepia(0..1) с шагом 0.1;
-// Для эффекта «Марвин» — filter: invert(0..100%) с шагом 1%;
-// Для эффекта «Фобос» — filter: blur(0..3px) с шагом 0.1px;
-// Для эффекта «Зной» — filter: brightness(1..3) с шагом 0.1;
-// Для эффекта «Оригинал» CSS-стили filter удаляются.
-// При выборе эффекта «Оригинал» слайдер скрывается.
-// При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%): слайдер, CSS-стиль изображения и значение поля должны обновляться.
-
-// // Обратите внимание, что при переключении фильтра, уровень эффекта должен сразу
-// сбрасываться до начального состояния, т. е. логика по определению уровня насыщенности
-// должна срабатывать не только при «перемещении» слайдера, но и при переключении фильтров.
